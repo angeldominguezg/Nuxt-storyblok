16904babf185b2a9818ecdb1ab5e0ff3ff6a03aa
Angelddg <angeldominguezg@gmail.com>
2018-04-03 17:14:19 -0400

    First Commit :)

---
+++ layouts/default.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <TheHeader />
+    <nuxt/>
+  </div>
+</template>
+<script>
+  import TheHeader from '@/components/TheHeader/TheHeader'
+  export default {
+    components: {
+      TheHeader
+    }
+  }
+</script>
+<style>
+* {
+  box-sizing: border-box;
+}
+body {
+  font-family: "Lato", sans-serif;
+}
+.main-content {
+  margin-top: 4.5rem;
+}
+</style>
